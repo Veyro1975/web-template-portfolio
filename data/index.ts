1c8ddba9a31c6833c1d5f3d11fe8793338059b98
Farrel Herdiyan <farrelherdiyan@gmail.com>
2025-06-05 02:48:15 +0700

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,150 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Quote", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Gw suka mempelajari hardware terbaru dan teknologinya ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Bahasa inggris gw cukup oke buat jadi pendengar",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "Growth to be better",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently studying computer science",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Email kontak yang punya template",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "My claseses friends",
+    des: "My friends from the same field of study",
+    img: "/batik.svg",
+    iconLists: ["/Logo_Universitas_Jambi.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Learning creating 2d game using unity engine",
+    img: "/Unity_SS.svg",
+    iconLists: ["/Unity_Logo.svg"],
+    link: "/ui.yoom.com",
+  },
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "The right man in the wrong place can make all the difference in the world.",
+    name: "G man",
+    title: "Half-Life 2",
+  },
+  {
+    quote:
+      "A hero need not speak. When he is gone, the world will speak for him.",
+    name: "-",
+    title: "Halo",
+  },
+  {
+    quote:
+      "You can’t undo what you’ve already done, but you can face up to it.",
+    name: "Frank Coleridge",
+    title: "Silent Hill:Downpour",
+  },
+  {
+    quote:
+      "Life is all about resolve. Outcome is secondary.",
+    name: "Waka",
+    title: "Okami",
+  },
+  {
+    quote:
+      "Hope is what makes us strong. It is why we are here. It is what we fight with when all else is lost.",
+    name: "Pandora",
+    title: "God of War 3",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Basic web development",
+    desc: "Its basic what do you expect?.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Veyro1975"
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link: "https://www.instagram.com/hrdiyan_?igsh=cXlkY2FpdmI2a2I5"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/farrel-herdiyan-b7637431a/"
+  },
+];
