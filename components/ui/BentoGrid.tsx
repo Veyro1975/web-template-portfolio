c3da0058ccacc0c696f02e02dda43d6ead8adc38
Farrel Herdiyan <farrelherdiyan@gmail.com>
2025-06-10 17:05:28 +0700

    Refactor Lottie import to use dynamic import with SSR disabled

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 import { cn } from "@/lib/utils";
-
 
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
